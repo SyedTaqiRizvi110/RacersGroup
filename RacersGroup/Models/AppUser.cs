dcd405363812d8b39c8862d3ee9ee8df1b1bdc50
Syed Taqi Rizvi <SP20MCS0005@MAJU.EDU.PK>
2023-02-15 16:37:26 +0500

    Add project files.

---
+++ RacersGroup/Models/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+
+namespace RacersGroup.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public int? Pace { get; set; }
+        public int? Milage { get; set; }
+        public string? ProfileImageUrl { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        [ForeignKey("Address")]
+        public int? AddressId { get; set; }
+        public Address? Address { get; set; }
+        public ICollection<Club> Club { get; set; }
+        public ICollection<Race> Race { get; set; }
+    }
+}
---
+++ RacersGroup/Models/State.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RacersGroup.Models
+{
+    public class State
+    {
+        [Key]
+        public int Id { get; set; }
+        public string StateName { get; set; }
+        public String StateCode { get; set; }
+    }
+}
